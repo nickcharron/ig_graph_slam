58cf09fd104832233e04b8b639c2dd9f24da6841
nickcharron <nicholas.c.charron@gmail.com>
2018-11-09 01:29:08 -0500

    adding SO(3) interpolation

--- include/ig_graph_slam/measurementtypes.hpp
+++ include/ig_graph_slam/measurementtypes.hpp
@@ -4,15 +4,16 @@
 #include <utility>
 #include <chrono>
 #include <math.h>
+#include <unsupported/Eigen/MatrixFunctions>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-//#include <pointmatcher/PointMatcher.h>
 
 #include <wave/utils/math.hpp>
 #include <wave/containers/measurement.hpp>
 
 using GPSMeasurement = wave::Measurement<std::pair<wave::Vec6, wave::Vec6>, uint>;
+using TransformMeasurement = wave::Measurement<std::pair<wave::Mat4, wave::Vec6>, uint>;
 using PCLLidarMeasurement = wave::Measurement<wave::PCLPointCloudPtr, uint>;
 //using LPMLidarMeasurement = wave::Measurement<boost::shared_ptr<PointMatcher<double>::DataPoints>, uint>;
 using PoseMeasurement = wave::Measurement<Eigen::Affine3d, uint>;
@@ -52,8 +53,33 @@ namespace wave
 
       first(5) = yaw1 + w2*delta;
       first(5) = wrapToPI(first(5));
-
       return std::make_pair(first, second);
+  }
+
+  inline decltype(TransformMeasurement::value) interpolate(const TransformMeasurement &m1,
+                                              const TransformMeasurement &m2,
+                                              const TimeType &t)
+  {
+    double w2 = 1.0 * (t - m1.time_point) / (m2.time_point - m1.time_point);
+
+    wave::Mat4 T1 = std::get<0>(m1.value);
+    wave::Mat4 T2 = std::get<0>(m2.value);
+    wave::Mat4 T;
+    wave::Vec6 std_dev = (1 - w2) * std::get<1>(m1.value) + w2 * std::get<1>(m2.value);
+
+    wave::Mat3 R1 = T1.block<3,3>(0,0);
+    wave::Mat3 R2 = T2.block<3,3>(0,0);
+    wave::Mat3 R = (R2 * R1.transpose()).pow(w2) * R1;
+
+    wave::Vec4 t1 = T1.rightCols<1>();
+    wave::Vec4 t2 = T2.rightCols<1>();
+    wave::Vec4 trans = (1-w2) * t1 + w2 * t2;
+
+    T.setIdentity();
+    T.block<3,3>(0,0) = R;
+    T.rightCols<1>()= trans;
+
+    return std::make_pair(T, std_dev);
   }
 
 }  // namespace wave
