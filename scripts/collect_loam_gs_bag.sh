3c63371b0c6d0454440e29d032daa2e2cbe4b62f
nickcharron <nicholas.c.charron@gmail.com>
2019-01-24 12:31:26 -0500

    adding scipts to control bag collection and processing via joystick

--- scripts/collect_loam_gs_bag.sh
+++ scripts/collect_loam_gs_bag.sh
@@ -2,12 +2,16 @@
 set -e
 
 u="$USER"
-savedir = "/home/$u/OPG_DEMO_JAN2019"
+savedir="/home/$u/OPG_DEMO_JAN2019/bags"
 
 if [ -d $savedir ]; then
   rm -rf $savedir
 fi
-mkdir $savedir
+mkdir -p $savedir
+
+echo "Starting loam_velodyne..."
+roslaunch loam_velodyne ig_loam_OPG_DEMO.launch &
+
 echo "Collecting bag file for loam initialized graph slam."
 echo "Saving to: "
 echo "$savedir/ig_scan_loam_OPG_JAN2019.bag"
@@ -15,4 +19,5 @@ rosbag record -O $savedir/ig_scan_loam_OPG_JAN2019.bag \
 /clock \
 /vvlp/velodyne_points \
 /hvlp/velodyne_points \
-/ig/loam/lidar_odom
+/ig/loam/lidar_odom \
+__name:=gs_loam_bag
---
+++ scripts/joy_mapping_controller.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import rospy
+import rospkg
+import os
+from sensor_msgs.msg import Joy
+from std_msgs.msg import Bool
+
+# Initialize variables
+buttons_array = [0, 0]
+collect_bag_btn_num = 7
+collect_bag_btn_sym = "START"
+stop_bag_btn_num = 6
+stop_bag_btn_sym = "BACK"
+
+def joy_CB(joy_msg):
+    global buttons_array
+    buttons_array = [joy_msg.buttons[collect_bag_btn_num],joy_msg.buttons[stop_bag_btn_num]]
+
+def print_instructions():
+
+    print ""
+    print "Press %s to start collecting a loam bag" % collect_bag_btn_sym
+    print "Press %s to stop collecting loam bag and run graph slam" % stop_bag_btn_sym
+    print ""
+
+def launch_subscribers():
+    rospy.init_node('joy_mapping_control')
+    rospy.Subscriber("/joy_teleop/joy", Joy, joy_CB )
+
+def check_buttons():
+
+    # Start collecting bag
+    if buttons_array[0] == 1:
+        while buttons_array[0] == 1:    # Wait for button to be released
+            pass
+        print "START button selected."
+        os.system('rosrun ig_graph_slam collect_loam_gs_bag.sh &')
+
+    if buttons_array[1] == 1:
+        while buttons_array[1] == 1:    # Wait for button to be released
+            pass
+        print "STOP button selected."
+        os.system('rosrun ig_graph_slam start_graph_slam.sh')
+
+def main():
+    # start node to subscribe to joy messages node end messages
+    launch_subscribers()
+
+    # check buttons and launch the appropriate file
+    while not rospy.is_shutdown():
+        check_buttons()
+    rospy.spin()
+
+if __name__ == '__main__':
+
+    print_instructions()
+
+    main()
