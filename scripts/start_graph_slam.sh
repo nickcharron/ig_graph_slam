772bd7dbe7c947877073597e7764ab7a1884777e
nickcharron <nicholas.c.charron@gmail.com>
2019-02-13 18:46:19 -0500

    changing directories

--- scripts/start_graph_slam.sh
+++ scripts/start_graph_slam.sh
@@ -11,19 +11,8 @@ rosnode kill /ig/loam/laserMapping
 rosnode kill /ig/loam/transformMaintenance
 rosnode kill /ig/loam/laserOdometry
 
-savedir="/home/$u/OPG_DEMO_JAN2019/pcds"
-if [ -d $savedir ]; then
-  rm -rf $savedir
-fi
-mkdir -p $savedir
-
 bagdir="/home/$u/OPG_DEMO_JAN2019/bags/ig_scan_loam_OPG_JAN2019.bag"
-# gsdir="$(rospack find ig_graph_slam)"
 echo "Starting ig_graph_slam with bag:"
 echo "$bagdir"
-echo "Saving maps to: "
-echo "$savedir"
-
-cd $savedir
-../../ig_catkin_ws/build/ig_graph_slam/ig_graph_slam_node
-# $gsdir/../../build/ig_graph_slam/ig_graph_slam_node
+cd ~/catkin_ws/
+./build/ig_graph_slam/ig_graph_slam_node
